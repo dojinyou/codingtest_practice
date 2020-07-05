8026587a54d02680cd1c6630fe17f1c68449bfe0
DOJIN <djyou128@gmail.com>
2020-07-06 00:31:25 +0900

    practice for hash

--- friendnetwork(4195).py
+++ friendnetwork(4195).py
@@ -13,23 +13,52 @@ Q. 친구 네트워크(4195): https://www.acmicpc.net/problem/4195
 """
 from sys import stdin
 
-for _ in range(int(stdin.readline().strip())):
-    f_dict ={}
-    for i in range(int(stdin.readline().strip())): # 친구 관계수 F < 100,000
-        f1, f2 = map(str, stdin.readline().split())
-        if f1 in f_dict :
-            if f2 in f_dict:
-                f_dict[f1].update(list(f_dict[f2])+[f2])
-                f_dict[f2].update(list(f_dict[f1])+[f1])
-            else:
-                f_dict[f1].add(f2)
-                f_dict[f2] = set(list(f_dict[f1])+[f1])
-        else :
-            if f2 in f_dict:
-                f_dict[f2].add(f1)
-                f_dict[f1] = set(list(f_dict[f2])+[f2])
-            else:
-                f_dict[f1] = set([f1, f2])
-                f_dict[f2] = set([f1, f2])
-        # print(f_dict[f1], f_dict[f2])
-        print(len(list(f_dict[f1].union(f_dict[f1]))))
+# # 시간초과
+# for _ in range(int(stdin.readline().strip())):
+#     set_list = []
+#     for i in range(int(stdin.readline().strip())): # 친구 관계수 F < 100,000
+#         f1, f2 = map(str, stdin.readline().split())
+#         f1_idx, f2_idx = -1, -1
+#         for i in range(len(set_list)):
+#             if f1 in set_list[i]:
+#                 f1_idx = i
+#             if f2 in set_list[i]:
+#                 f2_idx = i   
+#         if f1_idx != -1 :
+#             if f2_idx != -1 :
+#                 min_idx = min(f1_idx,f2_idx)
+#                 max_idx = max(f1_idx,f2_idx)
+#                 set_list[min_idx] = set_list[min_idx].union(set_list[max_idx])
+#                 set_list.pop(max_idx)
+#                 print(len(set_list[min_idx]))
+#             else :
+#                 set_list[f1_idx].add(f2)
+#                 print(len(set_list[f1_idx]))
+#         else:
+#             if f2_idx != -1 :
+#                 set_list[f2_idx].add(f1)
+#                 print(len(set_list[f2_idx]))
+#             else :
+#                 set_list.append(set([f1,f2]))
+#                 print(len(set_list[-1]))
+
+# # 메모리 초과
+# for _ in range(int(stdin.readline().strip())):
+#     f_dict ={}
+#     for i in range(int(stdin.readline().strip())): # 친구 관계수 F < 100,000
+#         f1, f2 = map(str, stdin.readline().split())
+#         if f1 in f_dict :
+#             if f2 in f_dict:
+#                 f_dict[f1].update(list(f_dict[f2])+[f2])
+#                 f_dict[f2].update(list(f_dict[f1])+[f1])
+#             else:
+#                 f_dict[f1].add(f2)
+#                 f_dict[f2] = set(list(f_dict[f1])+[f1])
+#         else :
+#             if f2 in f_dict:
+#                 f_dict[f2].add(f1)
+#                 f_dict[f1] = set(list(f_dict[f2])+[f2])
+#             else:
+#                 f_dict[f1] = set([f1, f2])
+#                 f_dict[f2] = set([f1, f2])
+#         print(len(list(f_dict[f1].union(f_dict[f1]))))
